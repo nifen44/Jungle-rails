85a456837b9c5c70defb8c5e6376968288fe3b03
nifen44 <huangwei19810803@gmail.com>
2023-06-28 16:07:50 -0600

    add admin security

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,16 @@
 class Admin::ProductsController < ApplicationController
 
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      # Perform authentication logic here
+      username == 'Jungle' && password == 'book'
+    end
+  end
+  
   def index
     @products = Product.order(id: :desc).all
   end
